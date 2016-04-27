44205b415aeffdf5066b2aec026ce1542de450aa
wfcsnzdm <530254427@qq.com>
2016-04-27 22:38:54 +0800

    cs

---
+++ cscompetition/cscompetition_files/data.js
@@ -0,0 +1,1 @@
+var defaultReply = [{text:"中央纪委监察部举报网站已收到您的举报，谢谢！",value:"93"}];
---
+++ cscompetition/cscompetition_files/easing.js
@@ -0,0 +1,11 @@
+<HTML>
+	<HEAD>
+		<TITLE>Service Unavailable - Fail to connect</TITLE>
+		<META http-equiv=Content-Type content="text-html; charset=UTF-8">
+	</HEAD>
+	<BODY>
+		<H1>Service Unavailable</H1>
+		The server is temporarily unable to service your request. Please try again later.
+		<P>Reference #60.75a9060e.75a90693.1446422929</P>
+	</BODY>
+</HTML>
